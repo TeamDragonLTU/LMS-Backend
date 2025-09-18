00920a0e44cdebf02b6c9964fbd4f63671e349d7
Felix Cederblad <felix_cederblad@hotmail.com>
2025-09-18 09:57:07 +0200

    Course controller and DTO

---
+++ LMS.Presentation/Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace LMS.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly IServiceManager serviceManager;
+
+        public CourseController(IServiceManager serviceManager)
+        {
+            this.serviceManager = serviceManager;
+        }
+        
+    }
+}
---
+++ LMS.Shared/DTOs/CourseDtos/CourseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace LMS.Shared.DTOs.CourseDtos
+{
+    public class CourseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime StartDate { get; set; }
+
+        // Navigationproperty till Module här i framtiden
+    }
+}
