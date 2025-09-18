fbbb81965cb9e8014e60cf58d71524783b019ced
Felix Cederblad <felix_cederblad@hotmail.com>
2025-09-18 10:29:10 +0200

    Course in Unit of Work

--- LMS.Infractructure/Repositories/UnitOfWork.cs
+++ LMS.Infractructure/Repositories/UnitOfWork.cs
@@ -5,9 +5,12 @@ namespace LMS.Infractructure.Repositories;
 public class UnitOfWork : IUnitOfWork
 {
     private readonly ApplicationDbContext context;
+    private readonly Lazy<ICourseRepository> _courseRepository;
+    public ICourseRepository Courses => _courseRepository.Value;
 
     public UnitOfWork(ApplicationDbContext context)
     {
+        _courseRepository = new Lazy<ICourseRepository>(() => new CourseRepository(context));
         this.context = context ?? throw new ArgumentNullException(nameof(context));
     }
 
