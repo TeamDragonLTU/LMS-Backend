df573c505cea3d171912ed254843dc2fd3803cfb
Felix Cederblad <felix_cederblad@hotmail.com>
2025-09-18 10:53:09 +0200

    Course: Register service, controller, put service in service manager

--- LMS.Presentation/Controllers/CourseController.cs
+++ LMS.Presentation/Controllers/CourseController.cs
@@ -5,14 +5,21 @@ namespace LMS.API
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class CourseController : ControllerBase
     {
-        private readonly IServiceManager serviceManager;
+        private readonly IServiceManager _serviceManager;
 
         public CourseController(IServiceManager serviceManager)
         {
-            this.serviceManager = serviceManager;
+            _serviceManager = serviceManager;
         }
-        
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCourses()
+        {
+            return Ok(await _serviceManager.CourseService.GetAllCoursesAsync(trackChanges: false));
+        }
+
     }
 }
