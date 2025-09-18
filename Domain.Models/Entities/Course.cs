a726daa9720fe368b51d9810e7920ebb1377e611
Felix Cederblad <felix_cederblad@hotmail.com>
2025-09-18 09:38:32 +0200

    Created entity DBSet and migration for Course

---
+++ Domain.Models/Entities/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models.Entities
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime StartDate { get; set; }
+
+    }
+}
--- LMS.Infractructure/Data/ApplicationDbContext.cs
+++ LMS.Infractructure/Data/ApplicationDbContext.cs
@@ -8,11 +8,14 @@ namespace LMS.Infractructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
     {
+
+        public DbSet<Course> Courses { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
-
+        
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
