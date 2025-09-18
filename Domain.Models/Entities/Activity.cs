90fdcf42c8a668c194823605955040e9d7e87d2d
Anna Eklund <anna.eklund@yahoo.se>
2025-09-18 13:46:20 +0200

    Add entities for Activity and ActivityType

---
+++ Domain.Models/Entities/Activity.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Models.Entities
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; } = null!;
+
+        public Guid ModuleID { get; set; } // Foreign key
+
+        // Navigation property
+        public Module Module { get; set; } = null!;
+
+    }
+}
