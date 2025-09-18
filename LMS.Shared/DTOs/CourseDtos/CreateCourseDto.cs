4b527b11bcfebe37604f51f4008d579367b48ca8
Felix Cederblad <felix_cederblad@hotmail.com>
2025-09-18 10:00:32 +0200

    CreateCourseDto

---
+++ LMS.Shared/DTOs/CourseDtos/CreateCourseDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Shared.DTOs.CourseDtos
+{
+    public class CreateCourseDto
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public string Description { get; set; } = null!;
+        public DateTime StartDate { get; set; }
+
+        // Navigationproperty till Module här i framtiden
+    }
+}
