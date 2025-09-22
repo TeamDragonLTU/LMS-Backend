c1ca6220e5e285d799279d412451bf7ca83abca1
Anna Eklund <anna.eklund@yahoo.se>
2025-09-22 11:40:58 +0200

    Add lazy dependency injection for repositories

--- LMS.API/Extensions/ServiceExtensions.cs
+++ LMS.API/Extensions/ServiceExtensions.cs
@@ -87,6 +87,11 @@ public static class ServiceExtensions
         services.AddScoped<IModuleRepository, ModuleRepository>();
         services.AddScoped<IActivityRepository, ActivityRepository>();
         services.AddScoped<IActivityTypeRepository, ActivityTypeRepository>();
+
+        services.AddScoped(provider => new Lazy<ICourseRepository>(() => provider.GetRequiredService<ICourseRepository>()));
+        services.AddScoped(provider => new Lazy<IModuleRepository>(() => provider.GetRequiredService<IModuleRepository>()));
+        services.AddScoped(provider => new Lazy<IActivityRepository>(() => provider.GetRequiredService<IActivityRepository>()));
+        services.AddScoped(provider => new Lazy<IActivityTypeRepository>(() => provider.GetRequiredService<IActivityTypeRepository>()));
     }
 
     public static void AddServiceLayer(this IServiceCollection services)
