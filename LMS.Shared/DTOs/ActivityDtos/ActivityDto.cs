b7b9d301916961e24034dc5064c1409d5e633a95
GustafssonJessica <jessica.gustafsson@live.se>
2025-09-22 14:42:54 +0200

    Add Acticity and ActivityType DTOs

---
+++ LMS.Shared/DTOs/ActivityDtos/ActivityDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs.ActivityDtos
+{
+    public class ActivityDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+    }
+}
---
+++ LMS.Shared/DTOs/ActivityTypeDto/ActivityTypeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs.ActivityTypeDto
+{
+    public class ActivityTypeDto
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+    }
+}
