9ce7e499878937637d35c373db1b9ac3a1f53071
GustafssonJessica <jessica.gustafsson@live.se>
2025-09-23 11:05:30 +0200

    Add Activity And ActivityType controllers

---
+++ LMS.Presentation/Controllers/ActivityTypeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Presentation.Controllers
+{
+    [Route("api/activitytype")]
+    [ApiController]
+    [Produces("application/json")]
+
+    public class ActivityTypeController: ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public ActivityTypeController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllActivityTypesAsync() 
+        {
+            return Ok(await _serviceManager.ActivityTypeService.GetAllActivityTypesAsync());    
+        }
+    }
+}
