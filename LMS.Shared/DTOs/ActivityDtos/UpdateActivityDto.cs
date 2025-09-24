9e91363d208fb1f26dbc0dbb713fac747e59f195
GustafssonJessica <jessica.gustafsson@live.se>
2025-09-24 15:19:26 +0200

    Add UpdateActivityDto

---
+++ LMS.Shared/DTOs/ActivityDtos/UpdateActivityDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs.ActivityDtos
+{
+    internal class UpdateActivityDto
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; } = null!;
+
+        // public Guid ModuleID { get; set; } // Foreign key
+        public Guid ActivityTypeID { get; set; } // Foreign key
+
+        // Navigation property
+        // public Module Module { get; set; } = null!;
+       // public ActivityType ActivityType { get; set; } = null!;
+
+
+    }
+}
