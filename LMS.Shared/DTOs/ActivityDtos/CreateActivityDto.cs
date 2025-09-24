86cad0817bf761275b133a878b01d3d3931b2506
GustafssonJessica <jessica.gustafsson@live.se>
2025-09-24 14:48:01 +0200

    Add ActivityTypeId in CreateActivityDto

--- LMS.Shared/DTOs/ActivityDtos/CreateActivityDto.cs
+++ LMS.Shared/DTOs/ActivityDtos/CreateActivityDto.cs
@@ -14,5 +14,6 @@ namespace LMS.Shared.DTOs.ActivityDtos
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public string Description { get; set; } = null!;
+        public Guid ActivityTypeId { get; set; } 
     }
 }
