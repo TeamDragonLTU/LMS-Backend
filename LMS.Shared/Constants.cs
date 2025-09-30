ed81e106f0be34cb6cd2686a90356145a6b4b21a
Anna Eklund <anna.eklund@yahoo.se>
2025-09-30 16:43:00 +0200

    Move Constants to Shared for use also in dtos

--- LMS.Shared/Constants.cs
+++ LMS.Shared/Constants.cs
@@ -1,7 +1,9 @@
-﻿namespace LMS.Infractructure.Data.Configurations
+﻿namespace LMS.Shared
 {
     public static class Constants
     {
+        public const int TextMinLength = 1;
+
         public const int NameMaxLength = 100;
         public const int DescriptionMaxLength = 500;
     }
