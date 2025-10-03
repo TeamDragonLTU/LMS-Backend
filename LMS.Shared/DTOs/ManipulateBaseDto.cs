98bc821b1451879178c3eaa03bdca0aa712449ce
Anna Eklund <anna.eklund@yahoo.se>
2025-10-03 18:01:39 +0200

    Delete regex since frameworks handles escaping and prevents XSS

--- LMS.Shared/DTOs/ManipulateBaseDto.cs
+++ LMS.Shared/DTOs/ManipulateBaseDto.cs
@@ -9,14 +9,12 @@ namespace LMS.Shared.DTOs
         [StringLength(Constants.NameMaxLength, MinimumLength = Constants.TextMinLength,
          ErrorMessage = "{0} must be between {2} and {1} characters.")]
         [NotOnlyWhitespace]
-        [RegularExpression(@"^[a-zA-Z0-9\s.\-/*"",:!?^]+$", ErrorMessage = "Name contains invalid characters. Only letters, numbers, spaces, and the following characters are allowed: . - / * , : ! ? ^ and double quotes")] // Against harmful strings, like html for XSS
         public string Name { get; init; } = string.Empty;
 
         [Required(ErrorMessage = "Description is a required field.")]
         [StringLength(Constants.NameMaxLength, MinimumLength = Constants.TextMinLength,
          ErrorMessage = "{0} must be between {2} and {1} characters.")]
         [NotOnlyWhitespace]
-        [RegularExpression(@"^[a-zA-Z0-9\s.\-/*"",:!?^]+$", ErrorMessage = "Description contains invalid characters. Only letters, numbers, spaces, and the following characters are allowed: . - / * , : ! ? ^  and double quotes")] // Against harmful strings, like html for XSS
         public string Description { get; init; } = string.Empty;
     }
 }
